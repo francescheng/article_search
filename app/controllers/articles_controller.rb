3f9c02621a9dfa9d4810ae3d40dfec78b3644e31
Frances Cheng <francesvcheng@gmail.com>
2021-07-12 21:26:51 -0700

    added article controller and model logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,34 @@
 class ArticlesController < ApplicationController
     def index
+        @articles = Article.all
     end
+
+    def show
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save 
+            redirect_to article_path(@article)
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :link, :body, :reading_time)
+    end
+
 end
