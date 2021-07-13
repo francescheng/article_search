e72ab6eaddf9fa128b6f04af6c24f97b68c7362c
Frances Cheng <francesvcheng@gmail.com>
2021-07-12 23:55:49 -0700

    added basic logic for scraper model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,15 +11,22 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        if params[:commit] == "Preview"
-            render :new
+        url = 'https://www.york.ac.uk/teaching/cws/wws/webpage1.html'
+        response = Scraper.process(url)
+        if response[:status] == :completed && response[:error].nil?
+            flash[:notice] = "Scraped url"
         else
-            @article = Article.new(article_params)
-            if @article.save 
-                redirect_to article_path(@article)
-            else
-                render :new
-            end
+            flash[:alert] = response[:error]
+        end
+
+    rescue StandardError => e
+        flash[:alert]= "Error: #{e}"
+        
+        @article = Article.new(article_params)
+        if @article.save 
+            redirect_to article_path(@article)
+        else
+            render :new
         end
     end
 
--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,2 +1,12 @@
 class Scraper < Kimurai::Base
+    @name = "scraper"
+    @engine = :selenium_chrome
+  
+    def self.process(url)
+        @start_urls = [url]
+        self.crawl!
+    end
+
+    def parse(response, url:, data: {})
+    end  
 end
